522afea6e4fe686b2718755191578e0af63cd4fd
AnjaliSinghhhh <anjalisingh24506@gmail.com>
2026-02-07 15:17:49 +0000

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,10 @@
 import sys
-import logging
+import os
+
+# Add the parent directory to sys.path to make 'src' importable
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()  # which file the error has occurred
@@ -16,5 +21,13 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Division by zero error occurred")
+        raise CustomException(e, sys)
 
 
